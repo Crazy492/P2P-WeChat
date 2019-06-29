f2bb8da707268125424a3f74948b8b66c406a33d
AddJun <1574118221@qq.com>
2019-06-29 11:48:25 +0800

    文件上传

--- app.js
+++ app.js
@@ -2,18 +2,33 @@ const Koa = require('koa');
 const views = require('koa-views');//视图渲染
 const path = require('path');//当前路径
 const static = require('koa-static');
-const bodyParser = require('koa-bodyparser');
+// const bodyParser = require('koa-bodyparser');
+// const bodyParser = require('koa-body');
+const body = require('koa-better-body');
 const router = require('./routers/index');
 const {io,getApp} = require('./utils/io');
+const cors = require('koa-cors');
+const IncomingForm = require('formidable');//用来保存后缀的
+const form = new IncomingForm();
 
+form.keepExtensions = true
+form.encoding = 'utf-8'
+form.uploadDir = path.join(__dirname, 'static/upload')
 
 let app = new Koa();
 
 getApp(app);
 io.attach(app);
+
+app.use(cors());
 app.use(views('views',{extension:'ejs'}));
 
-app.use(bodyParser());
+app.use(body({
+  //上传文件的制定路径
+  uploadDir: './static/upload',
+  IncomingForm: form
+}));
+
 app.use(static(__dirname + '/static'));
 app.use(router.routes());
 app.use(router.allowedMethods());
