1dc6efa320145be91653b73cca08ebd2c80119c2
AddJun <1574118221@qq.com>
2019-06-29 15:28:16 +0800

    why踢我

--- utils/io.js
+++ utils/io.js
@@ -37,12 +37,16 @@ io.on('login', async (ctx, data) => {
             server.setBroadcast(!0);//开启广播
             server.setTTL(128);
             server.send(`${IP}--${group}--${username}--${base64}--广播`, port, gbIP);
+            server.send(`${IP}--${group}--${username}--${base64}--广播`, port, gbIP);
+            server.send(`${IP}--${group}--${username}--${base64}--广播`, port, gbIP);
         }
         flag = 1;
     })
     io.on('disconnect', () => {
         server.setBroadcast(!0);//开启广播
         server.setTTL(128);
+        server.send(`${group}--${username}--退出`, port, gbIP);
+        server.send(`${group}--${username}--退出`, port, gbIP);
         server.send(`${group}--${username}--退出`, port, gbIP);
         console.log(`${group}--${username}--退出a `);
         db[group] = {};
@@ -62,22 +66,26 @@ io.on('login', async (ctx, data) => {
                     server.setBroadcast(0);//单播
                     server.setTTL(128);
                     server.send(`${IP}--${group}--${username}--${base64}--应答`, port, rinfo.address)
+                    server.send(`${IP}--${group}--${username}--${base64}--应答`, port, rinfo.address)
+                    server.send(`${IP}--${group}--${username}--${base64}--应答`, port, rinfo.address)
                     db[group][arr[2]] = { "IP": arr[0], "base64": arr[3] };
                     app._io.emit('updateList', db[group]);
                     console.log('你来了');
-                    break;
                 } else {
                     console.log('不回应');
                 }
+                break;
             case '应答':
                 db[group][arr[2]] = { "IP": arr[0], "base64": arr[3] };
                 app._io.emit('updateList', db[group]);
                 console.log('我知道你已经在了');
                 break;
             case '退出':
-                console.log('退出了');
+                if(arr[0]== group){
+                                    console.log('退出了');
                 delete db[arr[0]][arr[1]];
                 app._io.emit('smexit', db[arr[0]]);
+                }
                 break;
             case '私聊':
                 console.log(`${arr[0]}:${arr[1]}`, '12312312')
